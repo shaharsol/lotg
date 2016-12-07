b4000c1af3e902d5304f9104d3e5e6735ae8ec8e
Boris (Baruch) Kogan <boris.kogan81@gmail.com>
2016-12-07 14:25:56 +0000

    changed getRepoCommit fields

--- scms/gitlab.js
+++ scms/gitlab.js
@@ -35,8 +35,19 @@ module.exports = {
     var getPath = url + 'api/v3/projects/' + id + '/repository/commits?private_token=' + accessToken;
     request(getPath, function (error, response, body) {
       if (error){callback(error)}
-        if (!error && response.statusCode < 400) 
-          {callback(null,JSON.parse(body))}
+      if (!error && response.statusCode < 400){ 
+        var returnArray = [];
+        body = JSON.parse(body);
+        for (var i = 0; i < body.length; i++){
+          returnArray.push(
+            {
+              id : body[i]['id'],
+              author : body[i]['author_name'],
+              message : body[i]['message'] 
+            });
+          }
+          {callback(null, returnArray);}
+        }
       else {callback(null,response)}
     })
   },
--- test.js
+++ test.js
@@ -3,9 +3,9 @@ var util = require('util')
 // gitlab.getUserRepos('evm8NxEWJJNPqd6xuSta', function(err,projects){
 //     console.log(projects);
 // })
-// gitlab.listRepoCommits('evm8NxEWJJNPqd6xuSta', 2128337,  function(err,commits){
-//     console.log(commits)
-// })
-gitlab.commitsByUser('evm8NxEWJJNPqd6xuSta', 2128337, 'boris.kogan81@gmail.com', function(err,commits){
+gitlab.getRepoCommits('evm8NxEWJJNPqd6xuSta', 2128337,  function(err,commits){
     console.log(commits)
 })
+// gitlab.commitsByUser('evm8NxEWJJNPqd6xuSta', 2128337, 'boris.kogan81@gmail.com', function(err,commits){
+//     console.log(commits)
+// })
