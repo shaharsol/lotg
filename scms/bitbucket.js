e79df9699b2048f04c70c06feac76ac4bb12918e
Esther Stepansky <esstepansky@gatech.edu>
2016-12-07 18:12:53 +0200

    Added functionality to create commit list

--- scms/bitbucket.js
+++ scms/bitbucket.js
@@ -1,7 +1,7 @@
 var bitbucket = require('bitbucket-api');
 var util = require('util')
 var _ = require('underscore')
-
+var request = require('request')
 module.exports = {
   getUserRepos: function(accessToken,callback){
     var credentials = {username: accessToken.username, password: accessToken.password};
@@ -15,6 +15,7 @@ module.exports = {
           id: repo.slug,
           name: repo.name,
           url: repo.resource_uri
+
         })
       })
       callback(err,minified)
@@ -22,5 +23,31 @@ module.exports = {
     })
 
 
-  }
+  },
+  getRepoCommits: function(accessToken,repoID,callback){
+    var url =util.format( 'https://api.bitbucket.org/1.0/repositories/%s/%s/changesets?limit=10','C-Lion',repoID);
+
+    request(url,{auth:{user: accessToken.username,pass:accessToken.password}},function(error,response,body){
+      if(error){
+        callback(error)
+      }else if(response.statusCode > 300){
+        callback(body)
+      }else{
+        var data = JSON.parse(body);
+        var commits = [];
+        _.each(data.changesets,function(commit){
+          commits.push({
+            id: commit.raw_node,
+            message: commit.message,
+            author: commit.author
+          })
+        })
+        callback(null,commits)
+      }
+    })
+
+
+
+
+}
 }
