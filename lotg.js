ea1f464ce3d9e1c65acb5566860071e772257858
Shahar Solomianik <shahar@gnift.com>
2016-12-07 11:10:03 +0200

    basic work

--- lotg.js
+++ lotg.js
@@ -1,5 +1,24 @@
+var config;
 module.exports = {
   getUserRepos: function(accessToken,callback){
-    
+    switch(config.scm){
+      case 'github':
+      github.getUserRepos(accessToken,callback);
+      break;
+      case 'bitbucket':
+      bitbucket.getUserRepos(accessToken,callback);
+      break;
+      case 'gitlab':
+      gitlab.getUserRepos(accessToken,callback);
+      break;
+    }
+  },
+
+  init: function(config){
+    this.config = config;
+  },
+
+  getConfig: function(){
+    return this.config;
   }
 }
