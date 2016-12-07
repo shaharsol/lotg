8c74375d403d3df51fa9716c371069a900e9a9e7
guy <theguyshemesh@gmail.com>
2016-12-07 12:11:59 +0200

    getUserRepos in github

--- scms/github.js
+++ scms/github.js
@@ -1,5 +1,15 @@
-module.expots = {
-  getUserRepos: function(accessToken,callback){
-
+var GitHubApi = require('github');
+var github = new GitHubApi({});
+var util = require('util')
+module.exports = {
+    getUserRepos: function(accessToken,callback){
+        github.authenticate({
+            type: "oauth",
+            token: accessToken
+        });
+      github.repos.getAll({},function(err,res){
+          //console.log(util.inspect(res))
+          callback(err,res);
+      })
   }
 }
