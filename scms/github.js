7e2381e4c8a7f3206b10c7d8400e900d75bc66a0
guyshe <guyshe@campus.technion.ac.il>
2016-12-07 17:57:30 +0200

    get commit for repo function works

--- scms/github.js
+++ scms/github.js
@@ -19,32 +19,38 @@ module.exports = {
                       })
                   }
               })
-              //console.log(util.inspect(repos))
+              console.log(util.inspect(repos))
               callback(err,repos)
           })
       },
      getUserCommits: function(accessToken,callback){
+         var commitArray=[]
          github.authenticate({
              type: "oauth",
              token: accessToken
          })
          var author
          github.users.get({},function(err,user){
-             author = user
+            author = user
             // console.log('user is %s',util.inspect(user))
             github.repos.getAll({},function(err,res){
+            _.each(res,function(repo,repoKey){
+                console.log('--------------------------------------------------------------------------------------------')
+                console.log(util.inspect(repo))
                github.repos.getCommits({
                     owner: res[0].owner.login,
                     repo: res[0].name,
                     author: author.login
                },function(err,commits){
-                    // console.log('------------------------------------------------------------------------------------')
-                     console.log(util.inspect(commits))
-                    callback(err,commits)
+                       commitArray.push(commits)
+                    //    console.log('--------------------------------------------------------------------------------------------')
+                    //    console.log(util.inspect(commits))
                })
             })
          })
-     },
+         callback(err,commitArray)
+     })
+ },
      getUserCommitNum: function(accessToken,callback){
              var numOfCommits=0
              github.authenticate({
@@ -65,12 +71,81 @@ module.exports = {
                               if(key!='meta')
                                 numOfCommits++
                           })
-                          //conosle.log()
                           console.log(numOfCommits)
                           callback(err,numOfCommits)
                       })
                     //   console.log(util.inspect(numOfCommits))
              })
         })
+    },
+/*    ////////////////NOW REPLACING IT!
+    getRepoCommits: function(accessToken,repo,callback){
+            github.authenticate({
+                type: "oauth",
+                token: accessToken
+            })
+            github.repos.getAll({},function(err,res){
+            commits=[]
+            _.each(res,function(item,key){
+                if(item.id ==repo){
+                    github.repos.getCommits({
+                         owner: res[0].owner.login,
+                         repo: res[0].name,
+                    },function(err,commitArray){
+                        console.log(util.inspect(commit))
+                        _.each(commitArray,function(commit,key){
+                            if(key!= 'meta'){
+                                github.repos.getCommitComment({
+                                    owner:res[0].owner.login,
+                                    repo: res[0].name,
+                                    id: commit[0].sha
+                                },function(err,comment){
+                                    commits.push({
+                                        id: commit.sha,
+                                        author: item.name,
+                                        message: comment
+                                    })
+                                    console.log(util.inspect(comment))
+                                })
+                            }
+                        })
+                    })
+                })
+                callback(err,commits)
+            }
+        }
     }
+}
+*/
+
+
+getRepoCommits: function(accessToken,repo,callback){
+    github.authenticate({type: "oauth", token: accessToken})
+    github.repos.getAll({},function(err,res){
+        _.each(res,function(item,key){
+            if(key!='meta'&&item.id==repo){
+                github.repos.getCommits({owner: item.owner.login,repo: item.name,
+                },function(err,commitArray){
+                    commits=[]
+                    _.each(commitArray,function(commit,commitKey){
+                        if(commitKey!='meta'){
+                            commits.push({
+                                id: commit.sha,
+                                author: commit.commit.author.name,
+                                message: commit.commit.message
+                            })
+                        }
+                        // console.log(util.inspect(commit))
+                        // console.log(commit.commit.message)
+                        // console.log(commit.commit.author.name)
+                    })
+                    console.log(util.inspect(commits));
+                    callback(err,commits);
+                })
+            }
+        })
+    })
+
+}
+
 }
