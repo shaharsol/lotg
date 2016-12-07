1d9151e2fd8091789cd9519f93556f783a7418d3
guy <theguyshemesh@gmail.com>
2016-12-07 12:23:37 +0200

    merge

--- test.js
+++
@@ -1,6 +0,0 @@
-var gitlab = require ('./scms/gitlab.js');
-var util = require('util')
-gitlab.getUserRepos('evm8NxEWJJNPqd6xuSta', function(err,projects){
-    console.log(util.inspect(projects))
-    
-})
